435d649a0d3de39ee617d3f5960b6130b2772062
Danylo <31706307+JokerFunny@users.noreply.github.com>
2022-01-31 22:32:41 +0200

    - Update links.

--- rozklad-app/src/services/subjectsInfo.ts
+++ rozklad-app/src/services/subjectsInfo.ts
@@ -81,6 +81,8 @@ const subjects = [
                 type: LessonType.Lecture,
                 lecturer: "Корнієнко Б. Я.",
                 links: [
+                    { name: 'Telegram', link: 'https://t.me/+RNTV09xpZXFlmL-L' },
+                    { name: 'Zoom', link: 'https://us02web.zoom.us/j/2539419529?pwd=cUxoSkhNdEpYbzhzRkRjZWs4MU1nZz09' }
                 ],
                 isSelectable: true
             },
@@ -90,6 +92,8 @@ const subjects = [
                 type: LessonType.Lab,
                 lecturer: "Корнієнко Б. Я.",
                 links: [
+                    { name: 'Telegram', link: 'https://t.me/+RNTV09xpZXFlmL-L' },
+                    { name: 'Zoom', link: 'https://us02web.zoom.us/j/2539419529?pwd=cUxoSkhNdEpYbzhzRkRjZWs4MU1nZz09' }
                 ],
                 isSelectable: true
             },
@@ -123,6 +127,8 @@ const subjects = [
                 type: LessonType.Lecture,
                 lecturer: "",
                 links: [
+                    { name: 'Telegram', link: 'https://t.me/+pS0JOhWcWFtlMzY6' },
+                    { name: 'e-mail', link: 'education@gen.tech' }
                 ],
                 isSelectable: true
             },
@@ -132,6 +138,8 @@ const subjects = [
                 type: LessonType.Lab,
                 lecturer: "",
                 links: [
+                    { name: 'Telegram', link: 'https://t.me/+pS0JOhWcWFtlMzY6' },
+                    { name: 'e-mail', link: 'education@gen.tech' }
                 ],
                 isSelectable: true
             },
@@ -188,11 +196,35 @@ const subjects = [
                 isSelectable: false
             },
             {
+                name: "PAT-P",
+                viewName: "Інтелектуальна власність та патентознавство-2. Патентознавство та набуття прав",
+                type: LessonType.Practical,
+                lecturer: "Гаврушкевич Наталія Валеріївна",
+                links: [
+                    { name: 'Classroom', link: 'https://classroom.google.com/u/1/c/NDQ5MTY0NDM1NDcw' },
+                    { name: 'Telegram', link: 'https://t.me/+i0J6URn4Tuw2NWQy' }
+                ],
+                isSelectable: false
+            },
+            {
                 name: "SIR-L",
                 viewName: "Сталий іноваційний розвиток",
                 type: LessonType.Lecture,
                 lecturer: "Бендюг Владислав Іванович",
                 links: [
+                    { name: 'Telegram', link: 'https://t.me/+CN9T7bUdUAxkMjEy' },
+                    { name: '', link: 'https://www.xn--90aecd4bzg.xn--j1amh/sd/innosd' }
+                ],
+                isSelectable: false
+            },
+            {
+                name: "SIR-P",
+                viewName: "Сталий іноваційний розвиток",
+                type: LessonType.Practical,
+                lecturer: "Бендюг Владислав Іванович",
+                links: [
+                    { name: 'Telegram', link: 'https://t.me/+CN9T7bUdUAxkMjEy' },
+                    { name: 'Site', link: 'https://www.xn--90aecd4bzg.xn--j1amh/sd/innosd' }
                 ],
                 isSelectable: false
             },
@@ -204,7 +236,7 @@ const subjects = [
                 links: [
                     { name: 'Zoom', link: 'https://us04web.zoom.us/j/8894287667?pwd=L211bFZ1YWVqUlJvdTR5d2o1YkFqdz09' },
                     { name: 'Telegram', link: 'https://web.telegram.org/z/#-1549971772' },
-                    { name: 'Disk', link: 'https://classroom.google.com/u/1/c/Mzg3MDQ4NTgzMjAw' }
+                    { name: 'Classroom', link: 'https://classroom.google.com/u/1/c/Mzg3MDQ4NTgzMjAw' }
                 ],
                 isSelectable: false
             },
@@ -218,30 +250,13 @@ const subjects = [
                 isSelectable: false
             },
             {
-                name: "PAT-P",
-                viewName: "Інтелектуальна власність та патентознавство-2. Патентознавство та набуття прав",
-                type: LessonType.Practical,
-                lecturer: "Гаврушкевич Наталія Валеріївна",
-                links: [
-                    { name: 'Telegram', link: 'https://t.me/+i0J6URn4Tuw2NWQy' },
-                ],
-                isSelectable: false
-            },
-            {
-                name: "SIR-P",
-                viewName: "Сталий іноваційний розвиток",
-                type: LessonType.Practical,
-                lecturer: "Бендюг Владислав Іванович",
-                links: [
-                ],
-                isSelectable: false
-            },
-            {
                 name: "MMOS-L",
                 viewName: "Сучасні мобільні операційні системи",
                 type: LessonType.Lecture,
                 lecturer: "Корнієнко Б. Я.",
                 links: [
+                    { name: 'Telegram', link: 'https://t.me/+RNTV09xpZXFlmL-L' },
+                    { name: 'Zoom', link: 'https://us02web.zoom.us/j/2539419529?pwd=cUxoSkhNdEpYbzhzRkRjZWs4MU1nZz09' }
                 ],
                 isSelectable: true
             },
@@ -251,6 +266,8 @@ const subjects = [
                 type: LessonType.Lab,
                 lecturer: "Корнієнко Б. Я.",
                 links: [
+                    { name: 'Telegram', link: 'https://t.me/+RNTV09xpZXFlmL-L' },
+                    { name: 'Zoom', link: 'https://us02web.zoom.us/j/2539419529?pwd=cUxoSkhNdEpYbzhzRkRjZWs4MU1nZz09' }
                 ],
                 isSelectable: true
             },
@@ -284,6 +301,8 @@ const subjects = [
                 type: LessonType.Lecture,
                 lecturer: "",
                 links: [
+                    { name: 'Telegram', link: 'https://t.me/+pS0JOhWcWFtlMzY6' },
+                    { name: 'e-mail', link: 'education@gen.tech' }
                 ],
                 isSelectable: true
             },
@@ -293,6 +312,8 @@ const subjects = [
                 type: LessonType.Lab,
                 lecturer: "",
                 links: [
+                    { name: 'Telegram', link: 'https://t.me/+pS0JOhWcWFtlMzY6' },
+                    { name: 'e-mail', link: 'education@gen.tech' }
                 ],
                 isSelectable: true
             },
